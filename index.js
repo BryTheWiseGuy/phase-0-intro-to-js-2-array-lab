838e2c3c9f412cd53a285403d2319c5fd20c13b2
Brian Ashman <brian.ashman@comcast.net>
2023-03-04 00:08:34 -0700

    Complete Arrays Lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name){
+    return name = [...cats, "Broom"];
+}
+
+function prependCat(name) {
+    return name = ["Arnold", ...cats];
+}
+
+function removeLastCat(name) {
+    return name = cats.slice(0,2);
+}
+
+function removeFirstCat (name) {
+    return name = cats.slice(1);
+}
+
+
